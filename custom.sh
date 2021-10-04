2ed180c44961f241d32bab34e773e4473efe2677
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-04 13:18:33 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,8 +3,8 @@
 ## 本脚本搬运并模仿 liuqitoday
 # 
 
-dir_shell=/ql/config
-dir_script=/ql/scripts
+dir_shell=/opt/ql/config
+dir_script=/opt/ql/scripts
 config_shell_path=$dir_shell/config.sh
 extra_shell_path=$dir_shell/extra.sh
 code_shell_path=$dir_shell/code.sh
@@ -26,8 +26,8 @@ if (( $(echo "${config_size} < 100" | bc -l) )); then
     exit 0
 fi
 
-curl -s --connect-timeout 3 https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/shell/disableDuplicateTasksImplement.py > /ql/scripts/disableDuplicateTasksImplement.py
-curl -s --connect-timeout 3 https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/shell/wskey.py > /ql/scripts/wskey.py
+curl -s --connect-timeout 3 https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/shell/disableDuplicateTasksImplement.py > /opt/ql/scripts/disableDuplicateTasksImplement.py
+curl -s --connect-timeout 3 https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/shell/wskey.py > /opt/ql/scripts/wskey.py
 
 # 下载 extra.sh
 if [ ! -a "$extra_shell_path" ]; then
@@ -51,10 +51,10 @@ defaultNum=${defaultNum:-'1'}
 sed -i "s/CollectedRepo=(1)/CollectedRepo=($defaultNum)/g" $extra_shell_path
 
 # 将 extra.sh 添加到定时任务
-if [ "$(grep -c extra /ql/config/crontab.list)" = 0 ]; then
+if [ "$(grep -c extra /opt/ql/config/crontab.list)" = 0 ]; then
     echo "开始添加 task ql extra"
     # 获取token
-    token=$(cat /ql/config/auth.json | jq --raw-output .token)
+    token=$(cat /opt/ql/config/auth.json | jq --raw-output .token)
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"初始化任务","command":"ql extra","schedule":"15 0-23/8 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1624782068473'
 fi
 
@@ -81,10 +81,10 @@ repoNum=${repoNum:-'1'}
 sed -i "s/\$repo1/\$repo$repoNum/g" $code_shell_path
 
 # 将 code.sh 添加到定时任务
-if [ "$(grep -c code.sh /ql/config/crontab.list)" = 0 ]; then
+if [ "$(grep -c code.sh /opt/ql/config/crontab.list)" = 0 ]; then
     echo "开始添加 task code.sh"
     # 获取token
-    token=$(cat /ql/config/auth.json | jq --raw-output .token)
+    token=$(cat /opt/ql/config/auth.json | jq --raw-output .token)
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"格式化更新助力码","command":"task /ql/config/code.sh &","schedule":"20 * * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1626247939659'
 fi
 
@@ -101,12 +101,14 @@ fi
 
 
 # 将 bot 添加到定时任务
-if [ "$(grep -c bot /ql/config/crontab.list)" = 0 ]; then
+if [ "$(grep -c bot /opt/ql/config/crontab.list)" = 0 ]; then
     echo "开始添加 task ql bot"
     # 获取token
-    token=$(cat /ql/config/auth.json | jq --raw-output .token)
+    token=$(cat /opt/ql/config/auth.json | jq --raw-output .token)
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"拉取机器人","command":"ql bot","schedule":"13 14 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1626247933219'
 fi
 
-docker exec -it qinglong bash -c "task /ql/scripts/wskey.py"
+if [ "$(grep -c wskey.py /opt/ql/scripts/wskey.py)" = 1 ]; then
+    docker exec -it qinglong bash -c "task /ql/scripts/wskey.py"
+fi
 echo "所有任务安装完毕"

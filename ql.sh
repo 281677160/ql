ee752ebd45defbd8f8a457220490821ad5f6563b
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-05 19:50:25 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -49,6 +49,12 @@ fi
 sleep 3
 if [ -z "$(ls -A "dkql" 2>/dev/null)" ]; then
 	TIME g "已经有青龙面板，请登录面板设置好KEY"
+	read -p " [设置好KEY后，输入[ Y/y ]回车继续]： " MENU
+	case $MENU in
+		[Yy])
+			docker exec -it qinglong bash -c "$(curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/feverrun.sh)"
+		;;
+	esac
 else
 	TIME g "青龙面板安装完成，请过2分钟左右试用您宿主机 IP:5700 登录面板设置好KEY"
 	read -p " [设置好KEY后，输入[ Y/y ]回车继续]： " MENU

bc52c39a2e19fbfb1ab68f62abbd0c3807f45a5a
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-05 13:33:37 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -2,16 +2,16 @@
 #
 echo "安装依赖，所需时间比较长，请耐心等待..."
 
+npm install -g typescript
+npm install axios date-fns
+npm install crypto -g
+npm install jsdom
+npm install png-js
+npm install -g npm
+pnpm i png-js
+pip3 install requests
 apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
-cd /ql
-npm install png-js -S
-npm install date-fns -S
-npm install axios -S
-npm install crypto-js -S
-npm install ts-md5 -S
-npm install tslib -S
-npm install @types/node -S
-cd scripts && pnpm install jsdom
-cd /ql
+apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
 
 echo "所有依赖安装完毕"

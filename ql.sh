2eea1a7ce63dc76a96a495ee22fce1f95180adf7
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-05 19:40:38 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -30,13 +30,13 @@ docker ps -a > dkql
 if [[ `grep -c "whyour" dkql` -eq '0' ]]; then
   TIME g "正在安装青龙面板，请稍后..."
   docker run -dit \
-    -v /opt/ql/config:/ql/config \
-    -v /opt/ql/log:/ql/log \
-    -v /opt/ql/db:/ql/db \
-    -v /opt/ql/scripts:/ql/scripts \
-    -v /opt/ql/jbot:/ql/jbot \
-    -v /opt/ql/raw:/ql/raw \
-    -v /opt/ql/repo:/ql/repo \
+    -v $PWD/ql/config:/ql/config \
+    -v $PWD/ql/log:/ql/log \
+    -v $PWD/ql/db:/ql/db \
+    -v $PWD/ql/scripts:/ql/scripts \
+    -v $PWD/ql/jbot:/ql/jbot \
+    -v $PWD/ql/raw:/ql/raw \
+    -v $PWD/ql/repo:/ql/repo \
     -p 5700:5700 \
     --name qinglong \
     --hostname qinglong \
@@ -46,12 +46,15 @@ fi
 
 rm -fr dkql
 sleep 3
-   
-TIME g "青龙面板安装完成，请过2分钟左右试用您宿主机 IP:5700 登录面板设置好KEY"
-read -p " [设置好KEY后，输入[ Y/y ]回车继续]： " MENU
-case $MENU in
-	[Yy])
-		docker exec -it qinglong bash -c "$(curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/feverrun.sh)"
-	;;
-esac
+if [[ `grep -c "whyour" dkql` -eq '0' ]]; then
+	TIME g ""
+else
+	TIME g "青龙面板安装完成，请过2分钟左右试用您宿主机 IP:5700 登录面板设置好KEY"
+	read -p " [设置好KEY后，输入[ Y/y ]回车继续]： " MENU
+	case $MENU in
+		[Yy])
+			docker exec -it qinglong bash -c "$(curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/feverrun.sh)"
+		;;
+	esac
+fi
 exit 0

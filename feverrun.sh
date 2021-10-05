fc756db83c2240f8f211547d8ff171de79915a17
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-06 00:16:47 +0800

    Update feverrun.sh

--- feverrun.sh
+++ feverrun.sh
@@ -43,6 +43,7 @@ cp $sample_shell_path $config_shell_path
 # 判断是否下载成功
 config_size=$(ls -l $config_shell_path | awk '{print $5}')
 if (( $(echo "${config_size} < 100" | bc -l) )); then
+    echo
     TIME y "config.sh 下载失败"
     exit 0
 fi
@@ -58,6 +59,7 @@ cp $wskey_shell_path $dir_script/wskey.py
 # 判断是否下载成功
 wskey_size=$(ls -l $wskey_shell_path | awk '{print $5}')
 if (( $(echo "${wskey_size} < 100" | bc -l) )); then
+    echo
     TIME y "wskey.py 下载失败"
     exit 0
 fi
@@ -72,6 +74,7 @@ cp $extra_shell_path $dir_shell/extra.sh
 # 判断是否下载成功
 extra_size=$(ls -l $extra_shell_path | awk '{print $5}')
 if (( $(echo "${extra_size} < 100" | bc -l) )); then
+    echo
     TIME y "extra.sh 下载失败"
     exit 0
 fi
@@ -81,6 +84,7 @@ chmod -R +x $dir_shell
 
 # 将 extra.sh 添加到定时任务
 if [ "$(grep -c extra /ql/config/crontab.list)" = 0 ]; then
+    echo
     TIME g "开始添加任务 ql extra"
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
@@ -88,6 +92,7 @@ if [ "$(grep -c extra /ql/config/crontab.list)" = 0 ]; then
 fi
 
 if [ "$(grep -c wskey.py /ql/config/crontab.list)" = 0 ]; then
+    echo
     TIME g "开始添加任务 task wskey.py"
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
@@ -97,6 +102,7 @@ fi
 pip3 install requests
 
 if [[ "$(grep -c JD_WSCK=\"pin= /ql/config/env.sh)" = 1 ]]; then
+    echo
     TIME g "用WSKEY转换PT_KEY"
     task wskey.py
 fi
@@ -108,4 +114,4 @@ if [[ "$(grep -c JD_WSCK=\"pin= /ql/config/env.sh)" = 0 ]] && [[ "$(grep -c JD_C
     TIME y "没发现WSKEY或者PT_KEY，请注意设置好KEY，要不然脚本不会运行!"
 fi
 echo
-echo "所有任务安装完毕"
+TIME g "所有任务安装完毕"

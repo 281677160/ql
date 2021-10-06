f03a7dacf4587ea6425c0d8f6f931647e385c1af
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-06 11:52:49 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -36,39 +36,52 @@ if [[ -z "${Ubuntu}" ]] && [[ -z "${Debian}" ]]; then
 	echo
 	exit 1
 fi
+if [[ `dpkg -l | grep -c "docker"` -eq '0' ]]; then
+	echo
+	TIME y "检测到docker存在，是否重新安装?"
+	echo
+	TIME g "重新安装会把您现有的所以容器全部删除"
+	echo
+	read -p " [输入[ N/n ]退出安装，输入[ Y/y ]回车继续]： " ANDK
+	case $ANDK in
+		[Yy])
+			apt -qq install sudo
+			docker stop $(docker ps -a -q)
+			docker rm $(docker ps -a -q)
+			docker rmi $(docker images -q)
+			sudo -E apt-get -qq remove -y docker docker-engine docker.io containerd runc
+			sudo -E apt-get -qq remove -y docker  
+			sudo -E apt-get -qq remove -y --auto-remove docker
+			sudo -E apt-get -qq remove -y docker-ce
+			sudo -E apt-get -qq remove -y docker-ce-cli
+			sudo -E apt-get -qq remove -y docker-ce-rootless-extras
+			sudo -E apt-get -qq remove -y docker-scan-plugin
+			sudo -E apt-get -qq purge -y docker-ce
+			sudo rm -rf /var/lib/docker
 
-apt -qq install sudo
-
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
-sudo -E apt-get -qq remove -y docker docker-engine docker.io containerd runc
-sudo -E apt-get -qq remove -y docker  
-sudo -E apt-get -qq remove -y --auto-remove docker
-sudo -E apt-get -qq remove -y docker-ce
-sudo -E apt-get -qq remove -y docker-ce-cli
-sudo -E apt-get -qq remove -y docker-ce-rootless-extras
-sudo -E apt-get -qq remove -y docker-scan-plugin
-sudo -E apt-get -qq purge -y docker-ce
-sudo rm -rf /var/lib/docker
-
-sudo -E apt-get -qq update
-sudo -E apt-get -qq install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-if [[ "${Ubuntu}" == "ubuntu" ]]; then
-	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-else
-	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+			sudo -E apt-get -qq update
+			sudo -E apt-get -qq install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+			if [[ "${Ubuntu}" == "ubuntu" ]]; then
+				curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+			else
+				curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+			fi
+			sudo apt-key fingerprint 0EBFCD88
+			if [[ "${Ubuntu}" == "ubuntu" ]]; then
+				sudo add-apt-repository "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+			else
+				sudo add-apt-repository "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+			fi
+			sudo -E apt-get -qq update
+			sudo -E apt-get -qq install -y docker-ce docker-ce-cli containerd.io
+		;;
+		[Nn])
+			TIME r "退出安装程序!"
+			sleep 2
+			exit 1
+		;;
+	esac
 fi
-sudo apt-key fingerprint 0EBFCD88
-if [[ "${Ubuntu}" == "ubuntu" ]]; then
-	sudo add-apt-repository "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
-else
-	sudo add-apt-repository "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
-fi
-sudo -E apt-get -qq update
-sudo -E apt-get -qq install -y docker-ce docker-ce-cli containerd.io
-
-
 if [[ `dpkg -l | grep -c "docker"` -ge '1' ]]; then
 	echo
 	sudo service docker start

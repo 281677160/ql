9effdad1ee8a1cc8f82fce13d24cb987b2d1eb48
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-06 12:57:07 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -112,8 +112,12 @@ if [[ `docker ps -a | grep -c "hello-world"` -ge '1' ]]; then
 	docker stop $(docker ps -a -q)
 	docker rm $(docker ps -a -q)
 	docker rmi $(docker images -q)
+	echo
 	TIME g "docker拉取镜像成功"
+	echo
 else
+	echo
 	TIME y "docker拉取镜像失败，或许是docker安装有问题，请重新安装试试"
+	echo
 fi
 exit 0

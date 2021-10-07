c9855df22db8adeb72b6eccdecbfa0d94562f5d1
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-07 12:05:34 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -167,20 +167,21 @@ else
 	sleep 2
 	exit 1
 fi
-sleep 2
 echo
 echo
-TIME l "安装依赖，依赖必须安装，要不然脚本不运行"
-echo
-TIME y "但是用国内的网络安装比较慢，请尽量使用翻墙网络"
-echo
-TIME y "没翻墙条件的话，安装依赖太慢就换时间安装，我测试过不同时段有不同效果"
-echo
-TIME g "依赖安装时看到显示ERR!错误提示的，不用管，只要依赖能从头到尾的下载运行完毕就好了"
-echo
-TIME y "如果安装太慢，而想换时间安装的话，按键盘的 Ctrl+C 退出就行了，到时候可以使用我的一键独立安装依赖脚本来安装"
-echo
-sleep 15
-docker exec -it qinglong bash -c  "$(curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/npm.sh)"
+if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
+	TIME l "安装依赖，依赖必须安装，要不然脚本不运行"
+	echo
+	TIME y "但是用国内的网络安装比较慢，请尽量使用翻墙网络"
+	echo
+	TIME y "没翻墙条件的话，安装依赖太慢就换时间安装，我测试过不同时段有不同效果"
+	echo
+	TIME g "依赖安装时看到显示ERR!错误提示的，不用管，只要依赖能从头到尾的下载运行完毕就好了"
+	echo
+	TIME y "如果安装太慢，而想换时间安装的话，按键盘的 Ctrl+C 退出就行了，到时候可以使用我的一键独立安装依赖脚本来安装"
+	echo
+	sleep 15
+	docker exec -it qinglong bash -c  "$(curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/npm.sh)"
+fi
 
 exit 0

682b4aa398800734a8d3d4dbd60eb7c6a1513ea8
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-07 23:06:59 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -27,6 +27,58 @@ TIME() {
 	exit 1
 }
 
+if [[ `docker ps | grep -c "whyour"` -ge '1' ]]; then
+	echo
+	echo
+	echo
+	TIME z "脚本适用于（ubuntu、debian、openwrt）"
+	TIME z "自动检测docker，有则跳过，无则执行安装，如果是openwrt则请自行安装 docker"
+	TIME z "如果您以前安装有青龙的话，则自动删除您的青龙，全部推倒重新安装"
+	TIME z "如果您有要备份的青龙文件，请先备份再来安装，免得损失"
+	TIME z "如果有条件的话，最好使用翻墙网络来安装，要不然安装依赖的时候你会急死的"
+	TIME g "如要不能接受的话，请选择 3 回车退出程序!"
+	echo
+	echo
+	echo
+	TIME y "如要继续的话，请选择容器的网络类型!"
+	echo
+	TIME l " 1. bridge [默认类型] "
+	echo
+	TIME l " 2. host [一般为openwrt旁路由器才选择的]"
+	echo
+	TIME l " 3. 退出安装程序!"
+	echo
+	while :; do
+	read -p " [输入您的选择编码]： " SCQL
+	case $SCQL in
+		1)
+			QL_PORT="5700"
+			NETWORK="-p $QL_PORT:5700 \\"
+		break
+		;;
+		2)
+			NETWORK="--net host \\"
+		break
+		;;
+		3)
+			TIME r "您选择了退出编译程序"
+			exit 0
+		break
+    		;;
+    		*)
+			TIME r "提示：请输入正确的选择!"
+		;;
+	esac
+	done
+fi
+[[ "${QL_PORT}" == "5700" ]] && {
+	TIME g "请设置端口，默认为5700，不设置的话直接回车"
+	read -p " 请输入端口：" QL_PORT
+	QL_PORT=${QL_PORT:-"5700"}
+	TIME y "您端口为：$QL_PORT"
+	NETWORK="-p $QL_PORT:5700 \\"
+}
+
 if [[ -z "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
 	sudo -E apt-get -qq update
 	apt install -y sudo curl dpkg wget
@@ -73,41 +125,17 @@ if [[ `docker ps | grep -c "whyour"` -ge '1' ]]; then
 	echo
 	TIME g "检测到已有青龙面板，需要删除面板才能继续..."
 	echo
-	TIME y "如果要继续的话，请注意备份你原来的配置文件!"
+	TIME y "正在删除旧的青龙容器和镜像，请稍后..."
 	echo
-	while :; do
-	read -p " [输入[ N/n ]退出安装，输入[ Y/y ]回车继续]： " SCQL
-	case $SCQL in
-		[Yy])
-			echo
-			TIME y "正在停止您以前的青龙然后删除，请稍后...!"
-			echo
-			docker=$(docker ps|grep whyour) && dockerid=$(awk '{print $(1)}' <<<${docker})
-			images=$(docker images|grep whyour) && imagesid=$(awk '{print $(3)}' <<<${images})
-			docker stop -t=5 "${dockerid}"
-			docker rm "${dockerid}"
-			docker rmi "${imagesid}"
-		break
-		;;
-		[Nn])
-			TIME r "退出安装程序!"
-			sleep 2
-			exit 1
-		break
-    		;;
-    		*)
-			TIME r "提示：请输入正确的选择!"
-		;;
-	esac
-	done
+	docker=$(docker ps|grep whyour) && dockerid=$(awk '{print $(1)}' <<<${docker})
+	images=$(docker images|grep whyour) && imagesid=$(awk '{print $(3)}' <<<${images})
+	docker stop -t=5 "${dockerid}"
+	docker rm "${dockerid}"
+	docker rmi "${imagesid}"
 fi
-
 rm -rf /opt/ql
 rm -rf /root/ql
-sleep 3
 echo
-
-if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
 echo
 TIME g "正在安装青龙面板，请稍后..."
 echo
@@ -119,28 +147,11 @@ docker run -dit \
   -v $PWD/ql/jbot:/ql/jbot \
   -v $PWD/ql/raw:/ql/raw \
   -v $PWD/ql/repo:/ql/repo \
-  --net host \
+  ${NETWORK}
   --name qinglong \
   --hostname qinglong \
   --restart always \
   whyour/qinglong:latest
-else
-TIME g "正在安装青龙面板，请稍后..."
-echo
-docker run -dit \
-   -v /opt/ql/config:/ql/config \
-   -v /opt/ql/log:/ql/log \
-   -v /opt/ql/db:/ql/db \
-   -v /opt/ql/scripts:/ql/scripts \
-   -v /opt/ql/jbot:/ql/jbot \
-   -v /opt/ql/raw:/ql/raw \
-   -v /opt/ql/repo:/ql/repo \
-   -p 5700:5700 \
-   --name qinglong \
-   --hostname qinglong \
-   --restart always \
-   whyour/qinglong:latest
-fi
 
 if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
 	docker restart qinglong
@@ -148,7 +159,7 @@ if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
 	echo
 	TIME z "青龙面板安装完成"
 	echo
-	TIME g "请使用 IP:5700 在浏览器登录控制面板，然后在环境变量里添加好WSKEY或者PT_KEY"
+	TIME g "请使用 IP:$QL_PORT 在浏览器登录控制面板，然后在环境变量里添加好WSKEY或者PT_KEY"
 	echo
 	TIME y "您也可以不添加WSKEY或者PT_KEY，但是一定要登录管理面"
 	echo

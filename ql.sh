d1b78898240d997a340223ff92f1c43f9f9d61e0
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-07 18:47:08 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -33,7 +33,7 @@ if [[ -z "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
 fi
 
 if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
 		TIME y "没检测到docker，请先安装docker"
 		echo
@@ -41,7 +41,7 @@ if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
 		exit 1
 	fi
 else
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
 		TIME y "没检测到docker，正在安装docker，请稍后..."
 		echo
@@ -53,7 +53,7 @@ else
 	fi
 fi
 if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
 		TIME y "没检测到docker，请先安装docker"
 		echo
@@ -61,7 +61,7 @@ if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
 		exit 1
 	fi
 else
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
 		TIME y "没检测到docker，请先安装docker"
 		echo

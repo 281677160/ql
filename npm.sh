c3b65ebdd8b7b0a5afa0e823fd4148124a5db5c7
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-08 20:58:39 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 #
+
+TIME() {
+[[ -z "$1" ]] && {
+	echo -ne " "
+} || {
+     case $1 in
+	r) export Color="\e[31;1m";;
+	g) export Color="\e[32;1m";;
+	b) export Color="\e[34;1m";;
+	y) export Color="\e[33;1m";;
+	z) export Color="\e[35;1m";;
+	l) export Color="\e[36;1m";;
+      esac
+	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+	 }
+      }
+}
+
 TIME l "安装青龙脚本依赖"
 echo
 TIME y "最好使用翻墙网络来安装，或者更换虚拟机的源，要不然安装依赖的时候你会急死的"

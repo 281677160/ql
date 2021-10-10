922b84af34960ced82d86a7a747749c23d952bc6
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-10 16:56:45 +0800

    Update pvehy.sh

--- pvehy.sh
+++ pvehy.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+TIME() {
+[[ -z "$1" ]] && {
+	echo -ne " "
+} || {
+     case $1 in
+	r) export Color="\e[31;1m";;
+	g) export Color="\e[32;1m";;
+	b) export Color="\e[34;1m";;
+	y) export Color="\e[33;1m";;
+	z) export Color="\e[35;1m";;
+	l) export Color="\e[36;1m";;
+      esac
+	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+	 }
+      }
+}
+echo
+echo
 TIME g "正在更换源...!"
 echo
 echo

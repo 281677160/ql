a1a58e79d596aadd765aa4a3b46fe3622d54fbb0
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-10 15:54:02 +0800

    Update pvehy.sh

--- pvehy.sh
+++ pvehy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "正在更新源...!"
 echo '
 deb http://mirrors.aliyun.com/debian/ buster main non-free contrib
 deb http://mirrors.aliyun.com/debian/ buster-updates main non-free contrib
@@ -15,6 +16,7 @@ sed -i '1d' /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d/pve-enterprise.list
 echo "deb http://mirrors.ustc.edu.cn/proxmox/debian/pve bullseye pve-no-subscription" >/etc/apt/sources.list.d/pve-install-repo.list
 sed -i 's#http://download.proxmox.com#https://mirrors.tuna.tsinghua.edu.cn/proxmox#g' /usr/share/perl5/PVE/APLInfo.pm
+echo "下载PVE7.0源的密匙!"
 wget http://mirrors.ustc.edu.cn/proxmox/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 	if [[ $? -ne 0 ]];then
 		wget http://mirrors.ustc.edu.cn/proxmox/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
@@ -24,15 +26,18 @@ wget http://mirrors.ustc.edu.cn/proxmox/debian/proxmox-release-bullseye.gpg -O /
       exit 1
     fi
 	fi
+echo "更新源和安装常用软件"
+apt-get update && apt-get install lrzsz unzip net-tools curl screen uuid-runtime git vim -y
+echo "去除不用的内核"
+git clone https://ghproxy.com/https://github.com/jordanhillis/pvekclean.git && cd pvekclean && chmod +x pvekclean.sh
+./pvekclean.sh
+cd ../
+echo "更换DNS"
 echo '
 search com
 nameserver 223.5.5.5
 nameserver 114.114.114.114
 ' > /etc/resolv.conf
 sed -i '1d' /etc/resolv.conf
-service networking restart
-apt-get update && apt-get install lrzsz unzip net-tools curl screen uuid-runtime git vim -y
-git clone https://github.com/jordanhillis/pvekclean.git && cd pvekclean && chmod +x pvekclean.sh
-./pvekclean.sh
-cd ../
+echo "重启PVE，需要几分钟时间，请耐心等候..."
 reboot

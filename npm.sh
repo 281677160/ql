2c0e1f1dbda0c7570336d8b08ba7c5acfe590aaf
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-13 22:32:46 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -23,17 +23,17 @@ echo
 echo
 TIME l "安装青龙脚本依赖"
 echo
-TIME y "最好使用翻墙网络来安装，或者更换虚拟机的源，要不然安装依赖的时候你会急死的"
+TIME y "最好使用翻墙网络来安装，要不然安装依赖的时候你会急死的"
 echo
-TIME g "没翻墙条件，也不更换源的话，安装依赖太慢就换时间安装，我测试过不同时段有不同效果"
+TIME g "没翻墙条件，安装依赖太慢就换时间安装，我测试过不同时段有不同效果"
 echo
 TIME y "依赖安装时看到显示ERR!错误提示不用管，只要依赖能从头到尾的下载运行完毕就好了"
 echo
-TIME g "如果安装太慢，而想换时间安装的话，按键盘的 Ctrl+C 退出就行了，到时候可以使用我的一键独立安装依赖脚本来安装"
+TIME g "如果安装太慢，而想换时间安装的话，按键盘的 Ctrl+C 退出就行了"
 echo
 echo
 echo
-sleep 5
+sleep 8
 rm -rf npm.sh
 cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
 cd /ql

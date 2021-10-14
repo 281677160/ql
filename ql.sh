13eddc29da088d34e09b164e1f9300ef6b1b445e
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-14 12:32:26 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -113,11 +113,15 @@ elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
 	apt -y install sudo curl git
 	apt -y install net-tools
 elif [[ "$(. /etc/os-release && echo "$ID")" == "openwrt" ]]; then
-	TIME g "正在安装宿主机所需要的依赖，请稍后..."
 	QL_PATH="/opt"
 	XTong="openwrt"
-	opkg update
-	opkg install git-http
+	opkg list | awk '{print $1}' > Installed_PKG_List
+	if [[ ! "$(cat Installed_PKG_List)" =~ git-http ]]; then
+		TIME g "正在安装宿主机所需要的依赖，请稍后..."
+		opkg update
+		opkg install git-http
+	fi
+
 fi
 IP="$(ifconfig -a|grep inet|grep -v 127|grep -v 172|grep -v inet6|awk '{print $2}'|tr -d "addr:")"
 if [[ -z "${IP}" ]]; then

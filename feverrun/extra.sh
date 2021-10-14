4819c8d6adb89a74a5ddf1f75b49556cc7950f83
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-14 18:03:49 +0800

    Update extra.sh

--- feverrun/extra.sh
+++ feverrun/extra.sh
@@ -5,3 +5,5 @@
 ## 安装python依赖使用 pip3 install xxx
 
 ql repo https://github.com/feverrun/my_scripts.git "jd_|jx_|gua|jddj|getCookie|getJDCookie" "backUp" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
+wget -q -P /ql/scripts https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/sendNotify.js -O /ql/scripts/sendNotify.js
+wget -q -P /ql/scripts https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/sendNotify.py -O /ql/scripts/sendNotify.py

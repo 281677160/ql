445aab5882229fd0255447935efeb3ac5800c07b
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-15 18:06:45 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -21,22 +21,13 @@ TIME() {
 echo
 echo
 echo
-TIME l "安装青龙脚本依赖.."
+TIME l "安装依赖..."
 echo
-TIME y "最好使用翻墙网络来安装，要不然安装依赖的时候你会急死的"
+TIME y "安装依赖需要时间，请耐心等待!"
 echo
-TIME g "没翻墙条件，安装依赖太慢就换时间安装，我测试过不同时段有不同效果"
-echo
-TIME y "依赖安装时看到显示ERR!错误提示不用管，只要依赖能从头到尾的下载运行完毕就好了"
-echo
-TIME g "如果安装太慢，而想换时间安装的话，按键盘的 Ctrl+C 退出就行了"
-echo
-echo
-echo
-sleep 8
+sleep 3
 npm install -g cnpm --registry=https://registry.npm.taobao.org
 rm -rf npm.sh
-cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cnpm i && cnpm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
 cd /ql
 cnpm install -g typescript
 cd /ql
@@ -45,6 +36,8 @@ cd /ql
 cnpm install crypto -g
 cd /ql
 cnpm install ts-md5 -S
+cd /ql
+cnpm install tslib -S
 cd /ql
 cnpm install jsdom
 cd /ql
@@ -60,6 +53,8 @@ apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts &
 cd /ql
 apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
 cd /ql
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cnpm i && cnpm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
+cd /ql
 package_name="canvas png-js date-fns axios crypto-js ts-md5 tslib @types/node dotenv typescript fs require tslib"
 for i in $package_name; do
     case $i in
@@ -72,7 +67,5 @@ for i in $package_name; do
             ;;
     esac
 done
-echo
 TIME g "所有依赖安装完毕"
-echo
 exit 0

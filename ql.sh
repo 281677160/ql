0044b0b5c9e0c64d780832c2e3e13b7428515251
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-18 23:01:42 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -275,7 +275,7 @@ docker run -dit \
   whyour/qinglong:latest
 
 if [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
-	if [[ -n "$(ls -A "/opt/qlbeifen" 2>/dev/null)" ]]; then
+	if [[ -n "$(ls -A "/opt/qlbeifen1" 2>/dev/null)" ]]; then
 		docker cp /opt/qlbeifen1/config/env.sh qinglong:/ql/config/env.sh
 		docker cp /opt/qlbeifen1/db/env.db qinglong:/ql/db/env.db
 		docker cp /opt/qlbeifen1/config/auth.json qinglong:/ql/config/auth.json

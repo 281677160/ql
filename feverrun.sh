715f14422ddefd59ce8a960773f40ad02ba3a926
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-14 01:08:56 +0800

    Update feverrun.sh

--- feverrun.sh
+++ feverrun.sh
@@ -47,13 +47,13 @@ curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/feverrun/wskey.py > /ql
 # 授权
 chmod -R +x /ql/qlwj
 
-cp -Rf /ql/qlwj/feverrun/config.sample.sh /ql/config/config.sh
-cp -Rf /ql/qlwj/feverrun/config.sample.sh /ql/sample/config.sample.sh
-cp -Rf /ql/qlwj/feverrun/extra.sh /ql/config/extra.sh
-cp -Rf /ql/qlwj/feverrun/extra.sh /ql/sample/extra.sample.sh
-cp -Rf /ql/qlwj/feverrun/raw_jd_OpenCard.py /ql/scripts/raw_jd_OpenCard.py
-cp -Rf /ql/qlwj/feverrun/wskey.py /ql/scripts/wskey.py
-cp -Rf /ql/qlwj/feverrun/crypto-js.js /ql/scripts/crypto-js.js
+cp -Rf /ql/qlwj/config.sample.sh /ql/config/config.sh
+cp -Rf /ql/qlwj/config.sample.sh /ql/sample/config.sample.sh
+cp -Rf /ql/qlwj/extra.sh /ql/config/extra.sh
+cp -Rf /ql/qlwj/extra.sh /ql/sample/extra.sample.sh
+cp -Rf /ql/qlwj/raw_jd_OpenCard.py /ql/scripts/raw_jd_OpenCard.py
+cp -Rf /ql/qlwj/wskey.py /ql/scripts/wskey.py
+cp -Rf /ql/qlwj/crypto-js.js /ql/scripts/crypto-js.js
 echo
 echo
 TIME g "正在安装依赖，安装依赖需要时间，请耐心等候..."

349408465f085d513a69c749071f41392a1cea54
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-15 03:14:16 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -288,6 +288,9 @@ if [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
 	echo
 	docker restart qinglong > /dev/null 2>&1
 	sleep 3
+	clear
+	echo
+	echo
 	TIME y " "${IP}":"${QL_PORT}"  (IP检测因数太多，不一定准确，仅供参考)"
 	echo
 	TIME y "请确保您系统已放行${QL_PORT}端口，然后使用您的 IP:${QL_PORT} 在浏览器打开页面,登录青龙面板"

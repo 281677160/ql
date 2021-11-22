86b7de7106fd9372af577d9b0e4f0d02c155cc6d
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-22 15:14:38 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -285,8 +285,12 @@ if [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
 	clear
 	echo
 	TIME y "青龙面板安装完成，下一步进入安装任务程序，请耐心等候..."
-	docker exec -it qinglong bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/feverrun.sh)"	
-	docker restart qinglong > /dev/null 2>&1
+	docker exec -it qinglong bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/feverrun.sh)"
+	echo
+	if [[ ! -d /opt/ql/scripts/feverrun_my_scripts ]]; then
+		sleep 2
+		exit 1
+	fi
 	sleep 2
 	clear
 	echo

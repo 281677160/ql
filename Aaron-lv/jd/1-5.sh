f0f3322829c9a14982830fd450c3902f45f899c9
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-23 19:17:37 +0800

    Update 1-5.sh

--- Aaron-lv/jd/1-5.sh
+++ Aaron-lv/jd/1-5.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# 需要修改的变量,把1-5改成6-10就是提取6-10帐号的助力码
+# 需要修改的变量,把1-5改成6-10就是提取6-10帐号的助力码，类推，11-15、16-20，一个文件5个助力码，一个TG号
+
 export TG="1-5"
+export api_id="填写您Telegram的API的ID"
+export api_hash="填写您Telegram的API的密匙"
+
 
 export CRON="$(date +163764742%M%S)"
 export CRON1="$(date +163764761%M%S)"
 export CRON2="$(date +%M)"
 export token=$(cat /ql/config/auth.json | jq --raw-output .token)
-source /ql/jd/tg/"${TG}"
 [[ -d '/ql/log/jd_get_share_code' ]] && rm -rf /ql/log/jd_get_share_code/*
 task jd_get_share_code.js desi JD_COOKIE "${TG}"
 lOGName="$(ls -a /ql/log/jd_get_share_code |egrep -o [0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+.log |awk 'END {print}')"

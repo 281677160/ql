a5917271ced256aebacda55e10b62ab24c44bdd7
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-23 21:34:09 +0800

    Update tjzl.sh

--- Aaron-lv/tjzl.sh
+++ Aaron-lv/tjzl.sh
@@ -35,14 +35,14 @@ if [[ "$USER" == "root" ]]; then
 	read -p " ${scqlbianma}： " SCQL
 	case $SCQL in
 		1)
-			export QL_PORT="5700"
+			export QL_PORT="5600"
 			export QING_PORT="YES"
-			export NETWORK="-p ${QL_PORT}:5700"
+			export NETWORK="-p ${QL_PORT}:5600"
 		break
 		;;
 		2)
 			export NETWORK="--net host"
-			export QL_PORT="5700"
+			export QL_PORT="5600"
 		break
     		;;
     		*)
@@ -57,11 +57,11 @@ fi
 echo
 echo
 [[ "${QING_PORT}" == "YES" ]] && {
-	TIME g "请设置端口，默认端口为[5700]，不懂设置的话，直接回车使用默认[5700]端口"
+	TIME g "请设置端口，默认端口为[5600]，不懂设置的话，直接回车使用默认[5600]端口"
 	read -p " 请输入端口：" QL_PORT
-	export QL_PORT=${QL_PORT:-"5700"}
+	export QL_PORT=${QL_PORT:-"5600"}
 	TIME y "您端口为：${QL_PORT}"
-	export NETWORK="-p ${QL_PORT}:5700"
+	export NETWORK="-p ${QL_PORT}:5600"
 }
 echo
 echo

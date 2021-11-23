707ac1e8368cc40aad4f7b3f1dd3a05157d5cfcc
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-23 19:30:00 +0800

    Update Aaron-lv23.sh

--- Aaron-lv23.sh
+++ Aaron-lv23.sh
@@ -49,13 +49,8 @@ curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/Aaron-lv/disableDuplica
 curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/Aaron-lv/jd_Evaluation.py > /ql/qlwj/jd_Evaluation.py
 curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/Aaron-lv/jd_get_share_code.js > /ql/qlwj/jd_get_share_code.js
 curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/Aaron-lv/jdCookie.js > /ql/qlwj/jdCookie.js
-
 curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/Aaron-lv/jd/1-5.sh > /ql/jd/1-5.sh
 curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/Aaron-lv/jd/6-10.sh > /ql/jd/6-10.sh
-mkdir -p /ql/jd/tg
-curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/Aaron-lv/jd/tg/1-5 > /ql/jd/tg/1-5
-cp -Rf /ql/jd/tg/1-5 /ql/jd/tg/6-10
-
 chmod -R +x /ql/qlwj
 cp -Rf /ql/qlwj/config.sample.sh /ql/config/config.sh
 cp -Rf /ql/qlwj/config.sample.sh /ql/sample/config.sample.sh

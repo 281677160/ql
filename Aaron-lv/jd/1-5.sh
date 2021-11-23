72c36f38783699592d03803b8917da1ba2b73212
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-23 19:40:40 +0800

    Update 1-5.sh

--- Aaron-lv/jd/1-5.sh
+++ Aaron-lv/jd/1-5.sh
@@ -21,16 +21,13 @@ JXFACTORY="$(grep '/jxfactory' /ql/log/jd_get_share_code/${lOGName})"
 SGMH="$(grep '/sgmh' /ql/log/jd_get_share_code/${lOGName})"
 HEALTH="$(grep '/health' /ql/log/jd_get_share_code/${lOGName})"
 
-cat >/ql/scripts/${TG}.py <<-EOF
-#调度配置 0,1 0 * * 1 python3 /ql/scripts/${TG}.py
+cat >/ql/jd/${TG}.py <<-EOF
+#调度配置 0,1 0 * * 1 python3 /ql/jd/${TG}.py
 from telethon import TelegramClient
 import os
-
-
 current_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_path)
 client = TelegramClient("bot${TG}", "${api_id}", "${api_hash}", connection_retries=None).start()
-
 async def main():
     await client.send_message("@JDShareCodebot", "${FARM}")
     await client.send_message("@JDShareCodebot", "${PET}")
@@ -39,8 +36,6 @@ async def main():
     await client.send_message("@JDShareCodebot", "${SGMH}")
     await client.send_message("@JDShareCodebot", "${HEALTH}")
     await client.send_read_acknowledge("@JDShareCodebot")
-
-
 with client:
     client.loop.run_until_complete(main())
 EOF
@@ -51,7 +46,7 @@ if [ "$(grep -c ${TG}.py /ql/config/crontab.list)" = 0 ]; then
     echo
     echo "添加任务 [自动提交助力码${TG}]"
     echo
-    curl -s -H 'Accept: application/json' -H "Authorization: Bearer ${token}" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"自动提交助力码${TG}","command":"python3 /ql/scripts/${TG}.py","schedule":"0,1 0 * * 1"}' --compressed 'http://127.0.0.1:5700/api/crons?t=${CRON}'
+    curl -s -H 'Accept: application/json' -H "Authorization: Bearer ${token}" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"自动提交助力码${TG}","command":"python3 /ql/jd/${TG}.py","schedule":"0,1 0 * * 1"}' --compressed 'http://127.0.0.1:5700/api/crons?t=${CRON}'
 fi
 sleep 2
 echo

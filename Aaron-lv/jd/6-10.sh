88813ec679625e52ba79738e82e9f39f07545e43
281677160 <66651997+281677160@users.noreply.github.com>
2021-11-28 16:36:35 +0800

    Update 6-10.sh

--- Aaron-lv/jd/6-10.sh
+++ Aaron-lv/jd/6-10.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 需要修改的变量,把1-5改成6-10就是提取6-10帐号的助力码，类推，11-15、16-20，一个文件5个助力码，一个TG号
-# API提取网站< https://my.telegram.org/auth >，没有API的ID跟密匙的就别修改 api_id 跟 api_hash 的说明内容
+# API提取网站< https://my.telegram.org/auth >，没有API的ID跟密匙的就别修改 api_id 跟 api_hash 的说明内容，有就填写正确的ID跟密匙
 
 export TG="6-10"
 export api_id="填写您Telegram的API的ID"

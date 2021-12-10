101cb64554924cd0dbde36abef53835ed950586b
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-10 22:17:54 +0800

    Update tjzl.sh

--- Aaron-lv/tjzl.sh
+++ Aaron-lv/tjzl.sh
@@ -269,6 +269,7 @@ if [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
 		docker restart qinglong > /dev/null 2>&1
 		rm -fr ${QL_PATH}/qlbeifen1 > /dev/null 2>&1
 		docker exec -it qinglong bash -c "rm -rf /ql/qlwj"
+		bash /root/timesync.sh
 		sleep 2
 		clear
 		echo
@@ -336,6 +337,7 @@ if [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
 		docker restart qinglong > /dev/null 2>&1
 		rm -fr ${QL_PATH}/qlbeifen1 > /dev/null 2>&1
 		docker exec -it qinglong bash -c "rm -rf /ql/qlwj"
+		bash /root/timesync.sh
 		TIME g "任务安装完成"
 	fi
 else

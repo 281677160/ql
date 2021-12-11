f88f6fca1962dde62c57c34a731eefbfe2a1d208
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-12 01:05:32 +0800

    Update tjzl.sh

--- Aaron-lv/tjzl.sh
+++ Aaron-lv/tjzl.sh
@@ -279,7 +279,7 @@ if [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
 		docker restart qinglong > /dev/null 2>&1
 		rm -fr ${QL_PATH}/qlbeifen1 > /dev/null 2>&1
 		docker exec -it qinglong bash -c "rm -rf /ql/qlwj"
-		bash /root/timesync.sh
+		bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/timesync.sh)"
 		sleep 2
 		clear
 		echo
@@ -347,7 +347,7 @@ if [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
 		docker restart qinglong > /dev/null 2>&1
 		rm -fr ${QL_PATH}/qlbeifen1 > /dev/null 2>&1
 		docker exec -it qinglong bash -c "rm -rf /ql/qlwj"
-		bash /root/timesync.sh
+		bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/timesync.sh)"
 		TIME g "任务安装完成"
 	fi
 else

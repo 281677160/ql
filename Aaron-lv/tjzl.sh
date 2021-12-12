05b0e7b1281efa6c1dc8da688e69f52ccd833c54
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-12 15:05:53 +0800

    Update tjzl.sh

--- Aaron-lv/tjzl.sh
+++ Aaron-lv/tjzl.sh
@@ -71,19 +71,19 @@ if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
 	TIME g "正在安装宿主机所需要的依赖，请稍后..."
 	export QL_PATH="/opt"
 	yum -y update
-	yum -y install sudo wget git
+	yum -y install sudo wget git unzip
 	yum -y install net-tools.x86_64 subversion
 elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
 	TIME g "正在安装宿主机所需要的依赖，请稍后..."
 	export QL_PATH="/opt"
 	apt-get -y update
-	apt-get -y install sudo wget git
+	apt-get -y install sudo wget git unzip
 	apt-get -y install net-tools subversion
 elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
 	TIME g "正在安装宿主机所需要的依赖，请稍后..."
 	export QL_PATH="/opt"
 	apt -y update
-	apt -y install sudo wget git
+	apt -y install sudo wget git unzip
 	apt -y install net-tools subversion
 elif [[ "$(. /etc/os-release && echo "$ID")" == "openwrt" ]]; then
 	XTong="openwrt"

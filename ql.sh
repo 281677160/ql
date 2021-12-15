af8dc9db14753e21720ad652d9dcc3bc0d77206e
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-16 02:41:02 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -546,8 +546,6 @@ function up_nvjdc() {
   if [[ `docker ps -a | grep -c "nvjdc"` -ge '1' ]]; then
     rm -rf ${QL_PATH}/nvjdcbf
     echo "${Home}/rwwc" > ${Home}/rwwc
-  if [[ `docker ps -a | grep -c "nvjdc"` -ge '1' ]]; then
-    sleep 1
     docker restart qinglong > /dev/null 2>&1
     docker restart nolanjdc > /dev/null 2>&1
     sleep 1

55f38891c385d17503b2ffb07e415d98c6cde209
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-16 15:10:39 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,99 +1,98 @@
 #!/usr/bin/env bash
 
-TIME() {
-[[ -z "$1" ]] && {
-	echo -ne " "
-} || {
-     case $1 in
-	r) export Color="\e[31;1m";;
-	g) export Color="\e[32;1m";;
-	b) export Color="\e[34;1m";;
-	y) export Color="\e[33;1m";;
-	z) export Color="\e[35;1m";;
-	l) export Color="\e[36;1m";;
-      esac
-	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
-		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
-	 }
-      }
+# 字体颜色配置
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+Blue="\033[36m"
+Font="\033[0m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+OK="${Green}[OK]${Font}"
+ERROR="${Red}[ERROR]${Font}"
+
+function print_ok() {
+  echo
+  echo -e " ${OK} ${Blue} $1 ${Font}"
+  echo
+}
+function print_error() {
+  echo
+  echo -e "${ERROR} ${RedBG} $1 ${Font}"
+  echo
 }
 
-[[ ! "$USER" == "root" ]] && {
-	echo
-	TIME y "警告：请使用root用户操作!~~"
-	echo
-	exit 1
+function ECHOY() {
+  echo
+  echo -e "${Yellow} $1 ${Font}"
+  echo
+}
+function ECHOB() {
+  echo -e "${Blue} $1 ${Font}"
+}
+function ECHOG() {
+  echo
+  echo -e "${Green} $1 ${Font}"
+  echo
 }
 
-if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
-	export Aptget="yum"
-	yum -y update
-	yum install -y sudo wget curl
-	export XITONG="cent_os"
-elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
-	export Aptget="apt-get"
-	apt-get -y update
-	apt-get install -y sudo wget curl
-	export XITONG="ubuntu_os"
-elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
-	export Aptget="apt"
-	apt-get -y update
-	apt-get install -y sudo wget curl
-	export XITONG="debian_os"
-else
-	echo
-	TIME y "本一键安装docker脚本只支持（centos、ubuntu和debian）!"
-	echo
-	exit 1
+if [[ ! "$USER" == "root" ]]; then
+  print_error "警告：请使用root用户操作!~~"
+  exit 1
 fi
 
-if [[ `docker --version | grep -c "version"` -ge '1' ]]; then
-	echo
-	TIME y "检测到docker存在，是否重新安装?"
-	echo
-	TIME g "重新安装会把您现有的所有容器及镜像全部删除，请慎重!"
-	echo
-	while :; do
-	read -p " [输入[ N/n ]退出安装，输入[ Y/y ]回车继续]： " ANDK
-	case $ANDK in
-		[Yy])
-			TIME g "正在御载老版本docker"
-			export CHONGXIN="YES"
-			docker stop $(docker ps -a -q)
-			docker rm $(docker ps -a -q)
-			docker rmi $(docker images -q)
-			sudo "${Aptget}" remove -y docker docker-engine docker.io containerd runc
-			sudo "${Aptget}" remove -y docker
-			sudo "${Aptget}" remove -y docker-ce
-			sudo "${Aptget}" remove -y docker-ce-cli
-			sudo "${Aptget}" remove -y docker-ce-rootless-extras
-			sudo "${Aptget}" remove -y docker-scan-plugin
-			sudo "${Aptget}" remove -y --auto-remove docker
-			sudo rm -rf /var/lib/docker
-			sudo rm -rf /etc/docker
-			sudo rm -rf /lib/systemd/system/{docker.service,docker.socket}
-			rm /var/lib/dpkg/info/$nomdupaquet* -f
-		break
-		;;
-		[Nn])
-			echo
-			TIME r "您选择了退出程序!"
-			sudo rm -fr docker.sh
-			echo
-			sleep 3
-			exit 1
-		break
-    		;;
-    		*)
-			echo
-			TIME b "提示：请输入正确的选择!"
-			echo
-		;;
-	esac
-	done
-fi
-echo
-TIME y "正在安装docker，请耐心等候..."
+function system_check() {
+  if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
+    yum -y update
+    yum install -y sudo wget curl
+    [[ ${CHONGXIN} == "YES" ]] && unstall_centos_dk
+    ECHOY "正在安装docker，请耐心等候..."
+    install_centos_dk
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
+    apt-get -y update
+    apt-get install -y sudo wget curl
+    [[ ${CHONGXIN} == "YES" ]] && unstall_centos_dk
+    ECHOY "正在安装docker，请耐心等候..."
+    install_centos_dk
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
+    apt-get -y update
+    apt-get install -y sudo wget curl
+    [[ ${CHONGXIN} == "YES" ]] && unstall_centos_dk
+    ECHOY "正在安装docker，请耐心等候..."
+    install_centos_dk
+  else
+    print_error "本一键安装docker脚本只支持（centos、ubuntu和debian）!"
+    exit 1
+  fi
+}
+
+function jiance_dk() {
+  if [[ -x "$(command -v docker)" ]]; then
+    ECHOY "检测到docker存在，是否重新安装?"
+    ECHOB "重新安装会把您现有的所有容器及镜像全部删除，请慎重!"
+    while :; do
+    export CHONGXIN=""
+    read -p " [输入[ N/n ]退出安装，输入[ Y/y ]回车继续]： " ANDK
+    case $ANDK in
+     [Yy])
+       export CHONGXIN="YES"
+    break
+    ;;
+    [Nn])
+      export CHONGXIN="NO"
+      TIME r "您选择了退出安装程序!"
+      sleep 1
+      exit 1
+    break
+    ;;
+    *)
+      TIME b "提示：请输入正确的选择!"
+    ;;
+    esac
+    done
+  fi
+}
+
 function install_centos_dk() {
   sudo yum install -y yum-utils device-mapper-persistent-data lvm2
   yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
@@ -103,14 +102,15 @@ function install_centos_dk() {
   sudo systemctl restart docker
   sudo systemctl enable docker
   if [[ -x "$(command -v docker)" ]]; then
-    echo "docker安装成功"
+    print_ok "docker安装成功"
   else
-    echo "docker安装失败"
+    print_error "docker安装失败"
     exit 1
   fi
 }
 
 function unstall_centos_dk() {
+  ECHOY "正在御载docker..."
   docker stop $(docker ps -a -q)
   docker rm $(docker ps -a -q)
   docker rmi $(docker images -q)
@@ -138,14 +138,15 @@ function install_ubuntu_dk() {
   sudo systemctl daemon-reload
   sudo systemctl restart docker
   if [[ -x "$(command -v docker)" ]]; then
-    echo "docker安装成功"
+    print_ok "docker安装成功"
   else
-    echo "docker安装失败"
+    print_error "docker安装失败"
     exit 1
   fi
 }
 
 function unstall_ubuntu_dk() {
+  ECHOY "正在御载docker..."
   docker stop $(docker ps -a -q)
   docker rm $(docker ps -a -q)
   docker rmi $(docker images -q)
@@ -171,14 +172,15 @@ function install_debian_dk() {
   sudo systemctl daemon-reload
   sudo systemctl restart docker
   if [[ -x "$(command -v docker)" ]]; then
-    echo "docker安装成功"
+    print_ok "docker安装成功"
   else
-    echo "docker安装失败"
+    print_error "docker安装失败"
     exit 1
   fi
 }
 
 function unstall_debian_dk() {
+  ECHOY "正在御载docker..."
   docker stop $(docker ps -a -q)
   docker rm $(docker ps -a -q)
   docker rmi $(docker images -q)
@@ -187,18 +189,18 @@ function unstall_debian_dk() {
 }
 
 function hello_world() {
-  TIME g "测试docker拉取镜像是否成功"
+  ECHOY "测试docker拉取镜像是否成功"
   sudo docker run hello-world |tee build.log
   if [[ `docker ps -a | grep -c "hello-world"` -ge '1' ]] && [[ `grep -c "docs.docker" build.log` -ge '1' ]]; then
-    TIME g "测试镜像拉取成功，正在删除测试镜像..."
+    ECHOG "测试镜像拉取成功，正在删除测试镜像..."
     docker stop $(docker ps -a -q)
     docker rm $(docker ps -a -q)
     docker rmi $(docker images -q)
     rm -fr build.log
-    TIME y "测试镜像删除完毕"
+    ECHOY "测试镜像删除完毕"
   else
-    TIME y "docker虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的，或者容器网络问题"
-    TIME y "重启服务器后，用 sudo docker run hello-world 命令测试吧，能拉取成功就成了"
+    ECHOY "docker虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的，或者容器网络问题"
+    ECHOY "重启服务器后，用 sudo docker run hello-world 命令测试吧，能拉取成功就成了"
     rm -fr build.log
     sleep 2
     exit 1

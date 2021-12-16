097b2c1a2d745212b22753b80d0d880acf5b52a6
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-16 20:50:01 +0800

    Update Aaron-lv.sh

--- Aaron-lv.sh
+++ Aaron-lv.sh
@@ -47,7 +47,6 @@ curl -fsSL ${curlurl}/Aaron-lv/utils/jd_jxmcToken.js > /ql/scripts/utils/jd_jxmc
 curl -fsSL ${curlurl}/Aaron-lv/jd_cfd_sharecodes.ts > /ql/scripts/jd_cfd_sharecodes.ts
 curl -fsSL ${curlurl}/Aaron-lv/jd_jxmc_sharecodes.ts > /ql/scripts/jd_jxmc_sharecodes.ts
 curl -fsSL ${curlurl}/Aaron-lv/TS_USER_AGENTS.ts > /ql/scripts/TS_USER_AGENTS.ts
-curl -fsSL ${curlurl}/Aaron-lv/jd_cfd_loop.js > /ql/scripts/jd_cfd_loop.js
 
 TIME l "拉取auth.json"
 curl -fsSL ${curlurl}/Aaron-lv/auth.json > /ql/qlwj/auth.json
@@ -186,17 +185,6 @@ if [ "$(grep -c jd_cleancartAll.js /ql/config/crontab.list)" = 0 ]; then
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"清空购物车","command":"task jd_cleancartAll.js","schedule":"3 6,12,23 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1639110553549'
-fi
-sleep 1
-echo
-# 将 jd_cfd_loop.js 添加到定时任务
-if [ "$(grep -c jd_cfd_loop.js /ql/config/crontab.list)" = 0 ]; then
-    echo
-    TIME g "添加任务 [京喜财富岛热气球]"
-    echo
-    # 获取token
-    token=$(cat /ql/config/auth.json | jq --raw-output .token)
-    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"京喜财富岛热气球","command":"task jd_cfd_loop.js","schedule":"25 * * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1639205306669'
 fi
 task wskey.py |tee azcg.log
 echo

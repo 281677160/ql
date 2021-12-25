2f980b186fa4fb73a614982eaa6cdb1e6c6c9e37
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-25 14:07:21 +0800

    Update json.sh

--- json.sh
+++ json.sh
@@ -9,20 +9,7 @@ cat > ${Config}/Config.json << EOF
   "Title": "${NVJDCNAME}",
   //网站公告
   "Announcement": "本项目脚本收集于互联网。为了您的财产安全，请关闭京东免密支付。",
-  ///XDD PLUS Url  http://IP地址:端口/api/login/smslogin
-  "XDDurl": "",
-  ///xddToken
-  "XDDToken": "",
-  "AutoCaptchaCount": "5",
-  ///## 8. Push Plus官方网站：https://www.pushplus.plus
-  ///下方填写您的Token，微信扫码登录后一对一推送或一对多推送下面的token，只填" "PUSH_PLUS_TOKEN",
-  "PUSH_PLUS_TOKEN": "${PUSHPLUS}",
-  //下方填写您的一对多推送的 "群组编码" ，（一对多推送下面->您的群组(如无则新建)->群组编码）
-  "PUSH_PLUS_USER": "",
-
-  ///开启打印等待日志卡短信验证登陆 可开启 拿到日志群里回复
-  "Debug": "1",
-  ///青龙配置 注意 如果不要青龙  Config :[]
+  ///青龙配置
   "Config": [
     {
       //序号必填从1 开始

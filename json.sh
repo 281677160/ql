250cb471f4edfd055e0a1027821e6db5d2dd9992
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-25 14:17:53 +0800

    Update json.sh

--- json.sh
+++ json.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
 cat > ${Config}/Config.json << EOF
 {
-  ///最大支持几个网页
-  "MaxTab": "4",
+  ///浏览器最多几个网页
+  "MaxTab": "8",
   //网站标题
-  "Title": "${NVJDCNAME}",
+  "Title": "NolanJDCloud",
+  //回收时间分钟 不填默认3分钟
+  "Closetime": "5",
   //网站公告
-  "Announcement": "本项目脚本收集于互联网，为了您的财产安全，请关闭京东免密支付。",
-  ///多青龙配置
+  "Announcement": "为提高账户的安全性，请关闭免密支付。",
+  ///开启打印等待日志卡短信验证登陆 可开启 拿到日志群里回复 默认不要填写
+  "Debug": "",
+  ///自动滑块次数5次 5次后手动滑块 可设置为0默认手动滑块
+  "AutoCaptchaCount": "0",
+  ///XDD PLUS Url  http://IP地址:端口/api/login/smslogin
+  "XDDurl": "",
+  ///xddToken
+  "XDDToken": "",
+  ///登陆预警 0 0 12 * * ?  每天中午十二点 https://www.bejson.com/othertools/cron/ 表达式在线生成网址
+  "ExpirationCron": " 0 0 12 * * ?",
+  ///个人资产 0 0 10,20 * * ?  早十点晚上八点
+  "BeanCron": "0 0 10,20 * * ?",
+  // ======================================= WxPusher 通知设置区域 ===========================================
+  // 此处填你申请的 appToken. 官方文档：https://wxpusher.zjiecode.com/docs
+  // WP_APP_TOKEN 可在管理台查看: https://wxpusher.zjiecode.com/admin/main/app/appToken
+  // MainWP_UID 填你自己uid
+  ///这里的通知只用于用户登陆 删除 是给你的通知
+  "WP_APP_TOKEN": "",
+  "MainWP_UID": "",
+  // ======================================= pushplus 通知设置区域 ===========================================
+  ///Push Plus官方网站：http" //www.pushplus.plus  只有青龙模式有用
+  ///下方填写您的Token，微信扫码登录后一对一推送或一对多推送下面的token，只填" "PUSH_PLUS_TOKEN",
+  "PUSH_PLUS_TOKEN": "",
+  //下方填写您的一对多推送的 "群组编码" ，（一对多推送下面->您的群组(如无则新建)->群组编码）
+  "PUSH_PLUS_USER": "",
+  ///青龙配置  注意对接XDD 对接芝士 设置为"Config":[]
   "Config": [
     {
       //序号必填从1 开始
@@ -22,9 +49,10 @@ cat > ${Config}/Config.json << EOF
       "QL_SECRET": "${CLIENTID_SECRET}",
       //CK最大数量
       "QL_CAPACITY": ${CAPACITY},
-      //消息推送二维码
-      "QRurl": ""
+      ///建议一个青龙一个WxPusher 应用
+      "WP_APP_TOKEN": ""
     }
   ]
+
 }
 EOF

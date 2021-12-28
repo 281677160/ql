268a999de2a1e1f355ba166a873d02d56ca0fa34
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-28 11:42:44 +0800

    Update config.sample.sh

--- Aaron-lv/config.sample.sh
+++ Aaron-lv/config.sample.sh
@@ -406,7 +406,7 @@ export validate_num="" ##你需要的数值
 
 # Aaron-lv 环境变量
 ## 1、京东健康社区京豆兑换
-export JD_HEALTH_REWARD_NAME="" ##只能兑换京豆，填写纯数字20 10 5 3
+export JD_HEALTH_REWARD_NAME="20" ##只能兑换京豆，填写纯数字20 10 5 3
 
 # 不知名大佬环境变量
 ## 1、清空购物车
@@ -419,8 +419,9 @@ export CleanUsers=""
 export JD_BEAN_CHANGE_SENDNUM="10"
 ## 2、清空购物车
 ### 使用前请认真看对应注释：https://raw.githubusercontent.com/X1a0He/jd_scripts_fixed/main/jd_cart_remove.js
-### 当环境变量中存在JD_CART并设置为true时才会执行删除购物车
+### 当环境变量中存在JD_CART和rush_clean并设置为true时才会执行删除购物车
 export JD_CART="true"
+export rush_clean="true"
 ### 运行一次取消多全部已关注的商品。数字0表示不取关任何商品，默认20
 export JD_CART_REMOVESIZE="20"
 ### 是否清空，如果为false，则上面设置了多少就只删除多少条

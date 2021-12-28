ca74b2eea784e9f1eda4763a29b630703ec25981
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-29 01:02:51 +0800

    Update Aaron-lv.sh

--- Aaron-lv.sh
+++ Aaron-lv.sh
@@ -55,7 +55,7 @@ curl -fsSL ${curlurl}/Aaron-lv/config.sample.sh > /ql/qlwj/config.sample.sh
 TIME l "拉取extra.sh"
 curl -fsSL ${curlurl}/Aaron-lv/extra.sh > /ql/qlwj/extra.sh
 TIME l "拉取raw_jd_OpenCard.py"
-curl -fsSL ${curlurl}/Aaron-lv/raw_jd_OpenCard.py > /ql/qlwj/raw_jd_OpenCard.py
+curl -fsSL ${curlurl}/Aaron-lv/raw_jd_OpenCard.py > /ql/qlwj/jd_OpenCard.py
 TIME l "拉取wskey.py"
 curl -fsSL ${curlurl}/Aaron-lv/wskey.py > /ql/qlwj/wskey.py
 TIME l "拉取disableDuplicateTasksImplement.py"
@@ -75,7 +75,7 @@ cp -Rf /ql/qlwj/config.sample.sh /ql/config/config.sh
 cp -Rf /ql/qlwj/config.sample.sh /ql/sample/config.sample.sh
 cp -Rf /ql/qlwj/extra.sh /ql/config/extra.sh
 cp -Rf /ql/qlwj/extra.sh /ql/sample/extra.sample.sh
-cp -Rf /ql/qlwj/raw_jd_OpenCard.py /ql/scripts/raw_jd_OpenCard.py
+cp -Rf /ql/qlwj/raw_jd_OpenCard.py /ql/scripts/jd_OpenCard.py
 cp -Rf /ql/qlwj/wskey.py /ql/scripts/wskey.py
 cp -Rf /ql/qlwj/disableDuplicateTasksImplement.py /ql/scripts/disableDuplicateTasksImplement.py
 cp -Rf /ql/qlwj/jd_get_share_code.js /ql/scripts/jd_get_share_code.js
@@ -114,14 +114,14 @@ if [ "$(grep -c bot /ql/config/crontab.list)" = 0 ]; then
 fi
 sleep 1
 echo
-# 将 raw_jd_OpenCard.py 添加到定时任务
-if [ "$(grep -c raw_jd_OpenCard.py /ql/config/crontab.list)" = 0 ]; then
+# 将 jd_OpenCard.py 添加到定时任务
+if [ "$(grep -c jd_OpenCard.py /ql/config/crontab.list)" = 0 ]; then
     echo
     TIME g "添加任务 [JD入会开卡领取京豆]"
     echo
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
-    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"JD入会开卡领取京豆","command":"task raw_jd_OpenCard.py","schedule":"8 8,15,20 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1634041221437'
+    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"JD入会开卡领取京豆","command":"task jd_OpenCard.py","schedule":"8 8,15,20 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1634041221437'
 fi
 sleep 1
 echo

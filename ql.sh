7cf705bc9cd0263f73324df966114c656e8d75fe
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-30 01:21:09 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -224,7 +224,7 @@ function kaiqiroot_ssh() {
     ECHOGG "开启root用户ssh，方便使用工具连接服务器直接修改文件代码"
     bash -c "$(curl -fsSL ${curlurl}/ssh.sh)"
     judge "开启root用户ssh"
-    sleep 1
+    sleep 3
   fi
 }
 

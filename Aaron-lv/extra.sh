e9432ce8b67d84c499c96c1c8663d4ad39dc89fe
281677160 <66651997+281677160@users.noreply.github.com>
2022-05-12 17:20:38 +0800

    Update extra.sh

--- Aaron-lv/extra.sh
+++ Aaron-lv/extra.sh
@@ -4,14 +4,39 @@
 ## 安装node依赖使用 pnpm install -g xxx xxx
 ## 安装python依赖使用 pip3 install xxx
 
-## 拉取faker仓库
-ql repo ${GithubProxyUrl}https://github.com/shufflewzc/faker2.git "jd_|jx_|gua_|jddj_|getJDCookie" "activity|backUp|jd_disable.py|jd_get_share_code.js|jd_clean_car.js|jd_jdfactory.js|gua_cleancart.js|jd_jdfactory.js" "^jd[^_]|USER|function|utils|sendNotify|ZooFaker_Necklace.js|JDJRValidator_|sign_graphics_validate|ql"
-## 拉取青蛙仓库
-ql repo ${GithubProxyUrl}https://github.com/smiek2121/scripts.git "jd_|gua_" "gua_cleancart.js|gua_wealth_island_help.js|jd_sign_graphics.js|gua_ddworld.js|jd_joy_steal.js|jd_sendBeans.js|gua_wealth_island.js|jd_joy.js" "ZooFaker_Necklace.js|JDJRValidator_Pure.js|sign_graphics_validate.js|cleancart_activity.js|jdCookie.js|sendNotify.js"
-## 拉取小小仓库（去掉下面代码的 ## 为开启）
-## ql repo ${GithubProxyUrl}https://github.com/Aaron-lv/sync.git "jd_|jx_|getJDCookie" "activity|backUp|jd_get_share_code.js|getJDCookie.js|jd_bean_change.js|Coupon" "^jd[^_]|USER|utils|Notify" "jd_scripts"
-## 拉取樱花仓库助力脚本
-ql repo ${GithubProxyUrl}https://github.com/JDHelloWorld/jd_scripts.git "jd_fruit|jd_plantBean|jd_jdfactory|jd_dreamFactory|jd_pet|jd_health|jd_sgmh|jd_jxmc|jd_cfd|jd_88hb|jd_api_test" "activity|backUp|Coupon|enen|update|collect|game|shell|stock|getCoin|moreTask|jd_jxmc.js" "^jd[^_]|^USER|^TS|^JS|jdCookie|sendNotify|tools|utils|package|jd_validate_Worker"
 
-## 禁用重复任务（去掉下面代码的 ## 为开启）
-## task disableDuplicateTasksImplement.py
+## okyyds完整库拉取
+## ql repo https://github.com/okyyds/yyds.git "jd_|jx_|gua_|jddj_|jdCookie" "activity|backUp|icon" "^jd[^_]|USER|function|utils|sendNotify|ZooFaker_Necklace.js|JDJRValidator_|sign_graphics_validate|ql|JDSignValidator" "master"
+
+## faker2完整库拉取
+## ql repo https://github.com/shufflewzc/faker2.git "jd_|jx_|gua_|jddj_|getJDCookie" "activity|backUp|icon" "^jd[^_]|USER|function|utils|sendNotify|ZooFaker_Necklace.js|JDJRValidator_|sign_graphics_validate|ql" "main"
+
+## zero205完整库拉取
+## ql repo https://github.com/zero205/JD_tencent_scf.git "jd_|jx_|jdCookie" "backUp|icon" "^jd[^_]|USER|sendNotify|sign_graphics_validate|JDJR|JDSign|ql" "main"
+
+## 樱花完整库拉取
+## ql repo https://github.com/JDHelloWorld/jd_scripts "jd_|jx_" "backUp" "^jd[^_]|USER|^TS|utils"
+
+## 自助力的完整库拉取
+## ql repo https://github.com/feverrun/my_scripts.git "jd_|jx_|jddj|getCookie|getJDCookie" "backUp" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
+
+
+## 自助力的完整库（除了助力脚本外）
+ql repo https://github.com/feverrun/my_scripts.git "jd_|jx_|jddj|getCookie|getJDCookie" "activity|backUp|jd_dreamFactory|jd_factory|jd_plantBean|jd_health|jd_sgmh|jd_nzmh|jd_pet|jd_fruit|jd_cfd|jd_jxmc|jd_jxbean_exchange" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
+
+## faker2的单纯助力脚本（其他脚本没拉取）(助力码机器人：JDShareCodebot)
+## ql repo https://github.com/shufflewzc/faker2.git "jd_fruit|jd_jdfactory|jd_dreamFactory|jd_plantBean|jd_health|jd_pet|jd_petrw|jd_sgmh|jd_cfd|jd_jxmc" "activity|backUp|Coupon|enen|update|collect|game|shell|stock|getCoin|moreTask" "^jd[^_]|USER|function|utils|sendNotify|ZooFaker_Necklace.js|JDJRValidator_|package|sign_graphics_validate" "main"
+
+ql repo https://github.com/okyyds/yyds.git "jd_fruit|jd_jdfactory|jd_dreamFactory|jd_plantBean|jd_health|jd_pet|jd_petrw|jd_sgmh|jd_cfd|jd_jxmc|jd_exchangejxbeans" "activity|backUp|icon" "^jd[^_]|USER|function|utils|sendNotify|ZooFaker_Necklace.js|JDJRValidator_|sign_graphics_validate|package|JDSignValidator" "master"
+
+ql repo https://github.com/smiek2121/scripts.git "jd_|gua_" "gua_cleancart.js|gua_wealth_island_help.js|jd_sign_graphics.js|gua_ddworld.js|jd_joy_steal.js|jd_sendBeans.js|gua_wealth_island.js|jd_joy.js" "ZooFaker_Necklace.js|JDJRValidator_Pure.js|sign_graphics_validate.js|cleancart_activity.js|jdCookie.js|sendNotify.js"
+
+## zero205的单纯助力脚本（其他脚本没拉取）(助力码机器人：JDShareCodebot)，备用库，提交助力跟faker2同一个机器人，所以不需要同时拉取
+## ql repo https://github.com/zero205/JD_tencent_scf.git "jd_fruit|jd_jdfactory|jd_dreamFactory|jd_plantBean|jd_health|jd_pet|jd_petrw|jd_sgmh|jd_cfd|jd_jxmc" "activity|backUp|Coupon|enen|update|collect|game|shell|stock|getCoin|moreTask" "^jd[^_]|USER|sendNotify|sign_graphics_validate|JDJR|package|JDSign" "main"
+
+## 樱花的单纯助力脚本（其他脚本没拉取）(助力码机器人：JD_ShareCode)
+ql repo https://github.com/JDHelloWorld/jd_scripts.git "jd_fruit|jd_plantBean|jd_jdfactory|jd_dreamFactory|jd_pet|jd_health|jd_sgmh|jd_jxmc|jd_cfd|jd_88hb|jd_api_test|jd_sms_login" "activity|backUp|Coupon|enen|update|collect|game|shell|stock|getCoin|moreTask|jd_jxmc.js" "^jd[^_]|^USER|^TS|^JS|jdCookie|sendNotify|tools|utils|package|jd_validate_Worker"
+
+## 拉取WSKEY转换脚本
+ql repo https://github.com/Zy143L/wskey.git
+

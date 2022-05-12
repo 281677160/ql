bc6080685f510e9c0017d0018ed404f8a95f55f4
281677160 <66651997+281677160@users.noreply.github.com>
2022-05-12 19:08:46 +0800

    Update feverrun.sh

--- feverrun.sh
+++ feverrun.sh
@@ -49,8 +49,6 @@ TIME l "拉取extra.sh"
 curl -fsSL ${curlurl}/feverrun/extra.sh > /ql/data/qlwj/extra.sh
 TIME l "拉取jd_OpenCard.py"
 curl -fsSL ${curlurl}/feverrun/jd_OpenCard.py > /ql/data/qlwj/jd_OpenCard.py
-TIME l "拉取wskey.py"
-curl -fsSL ${curlurl}/feverrun/wskey.py > /ql/data/qlwj/wskey.py
 chmod -R +x /ql/data/qlwj
 cp -Rf /ql/data/qlwj/config.sample.sh /ql/data/config/config.sh
 cp -Rf /ql/data/qlwj/config.sample.sh /ql/sample/config.sample.sh
@@ -112,17 +110,20 @@ if [ "$(grep -c rmlog /ql/data/config/crontab.list)" = 0 ]; then
     token=$(cat /ql/data/config/auth.json | jq --raw-output .token)
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"每隔7天删除日志","command":"ql rmlog 7","schedule":"0 2 */7 * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1640581005650'
 fi
-task wskey.py
+ql repo https://github.com/Zy143L/wskey.git
+if [[ "$(grep -c JD_WSCK=\"pin= /ql/data/config/env.sh)" = 1 ]]; then
+  task Zy143L_wskey/wskey.py
+fi
 echo
 TIME y "拉取feverrun大佬的自动提交助力码脚本（要找库的作者过白名单）"
 echo
-rm -fr /ql/data/azcg.log
+rm -fr /ql/azcg.log
 ql extra |tee azcg.log
-if [[ `ls -a |grep -c "成功" /ql/data/azcg.log` -ge '1' ]]; then
-	rm -fr /ql/data/azcg.log
+if [[ `ls -a |grep -c "成功" /ql/azcg.log` -ge '1' ]]; then
+	rm -fr /ql/azcg.log
 else
 	TIME r "脚本安装失败,请再次执行一键安装脚本尝试安装，或看看青龙面板有没有[每x小时更新任务]，有的话执行这个拉取任务试试"
-	rm -fr /ql/data/azcg.log
+	rm -fr /ql/azcg.log
 	echo "Error" > /ql/data/config/Error
 fi
 exit 0

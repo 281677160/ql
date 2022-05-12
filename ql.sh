d02774ddd2fd2d51b140bc2c004ef4932fcbecf4
281677160 <66651997+281677160@users.noreply.github.com>
2022-05-12 19:12:20 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -998,7 +998,7 @@ memu() {
   ECHORR "安装过程会有重启docker操作，如不能接受，请退出安装"
   echo
   ECHOY " 请选择您要安装什么类型的任务库"
-  ECHOB " 1. TG机器人每周提交助力码库（shufflewzc/faker2和JDHelloWorld/jd_scripts）两个库"
+  ECHOB " 1. TG机器人每周提交助力码库（okyyds和JDHelloWorld）两个库"
   ECHOB " 2. 自动提交助力码库,要去库的作者那里提交资料过白名单（feverrun/my_scripts）单库"
   ECHOB " 3. 退出安装程序!"
   echo
